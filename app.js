ba6bc08c4c36bf30dcbc1fafb7563543ec9c1513
omelchenkoigor8388 <omelchenkoigor8388@gmail.com>
2023-02-13 19:03:01 +0200

    select_department

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 $.ajax({
-    type: 'POST',
-    dataType: 'json',
-    url: 'https://api.novaposhta.ua/v2.0/json/',
-    data: JSON.stringify({
-      modelName: 'Address',
-      calledMethod: 'getWarehouses',
-      methodProperties: {
-        CityName: 'миронівка',
-        Limit: 555
-      },
-      apiKey: 'd2f1d924423a9a17e82873f4e6922804'
-    }),
-    headers: {
-      'Content-Type': 'application/json'
+  type: 'POST',
+  dataType: 'json',
+  url: 'https://api.novaposhta.ua/v2.0/json/',
+  data: JSON.stringify({
+    modelName: 'Address',
+    calledMethod: 'getWarehouses',
+    methodProperties: {
+      CityName: 'кагарлик',
+      Limit: 555
     },
-    xhrFields: {
-      withCredentials: false 
-    },
-    success: function(texts) {
-      let text = texts.data
-
-      text.forEach(function(entry) {
-        console.log(entry.Description);
-      });
-    },
+    apiKey: 'd2f1d924423a9a17e82873f4e6922804'
+  }),
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  xhrFields: {
+    withCredentials: false 
+  },
+  success: function(texts) {
+    let text = texts.data
+    const dataForm = document.querySelector('#post_options')
+    text.forEach(function(entry) {
+      dataForm.innerHTML += `<option value="${entry.Description}">${entry.Description}</option>`
+    });
+  },
 });
-
