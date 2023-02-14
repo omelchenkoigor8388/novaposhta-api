2c1c2a0a79ac4e7e620de27f8a6e5fb534ab77cf
omelchenkoigor8388 <omelchenkoigor8388@gmail.com>
2023-02-14 15:16:52 +0200

    citi filter

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-$.ajax({
-  type: 'POST',
-  dataType: 'json',
-  url: 'https://api.novaposhta.ua/v2.0/json/',
-  data: JSON.stringify({
-    modelName: 'Address',
-    calledMethod: 'getWarehouses',
-    methodProperties: {
-      CityName: 'кагарлик',
-      Limit: 555
+document.querySelector("#city-options").addEventListener('input', e => {
+  console.log("мур")
+  const city = document.getElementById('city-options').value;
+  const dataForm = document.querySelector('#post_options')
+  dataForm.innerHTML = ''
+
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    url: 'https://api.novaposhta.ua/v2.0/json/',
+    data: JSON.stringify({
+      modelName: 'Address',
+      calledMethod: 'getWarehouses',
+      methodProperties: {
+        CityName: city,
+        Limit: 555
+      },
+      apiKey: 'd2f1d924423a9a17e82873f4e6922804'
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     },
-    apiKey: 'd2f1d924423a9a17e82873f4e6922804'
-  }),
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  xhrFields: {
-    withCredentials: false 
-  },
-  success: function(texts) {
-    let text = texts.data
-    const dataForm = document.querySelector('#post_options')
-    text.forEach(function(entry) {
-      dataForm.innerHTML += `<option value="${entry.Description}">${entry.Description}</option>`
-    });
-  },
-});
+    xhrFields: {
+      withCredentials: false 
+    },
+    success: function(texts) {
+      let text = texts.data
+      
+      text.forEach(function(entry) {
+        dataForm.innerHTML += `<option value="${entry.Description}">${entry.Description}</option>`
+      });
+    },
+  });
+})
