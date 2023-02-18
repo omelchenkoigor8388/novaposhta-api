d8f8ed479c1f95090eea5432449a077cb8750ecb
omelchenkoigor8388 <omelchenkoigor8388@gmail.com>
2023-02-19 01:01:24 +0200

    nova poshta API

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-document.querySelector("#city-options").addEventListener('input', e => {
-  const city = document.getElementById('city-options').value;
-  const dataForm = document.querySelector('#post-options')
-  dataForm.innerHTML = ''
+const cityOptions = document.querySelector("#city-options");
+const postOptions = document.querySelector("#post-options");
 
-  $.ajax({
-    type: 'POST',
-    dataType: 'json',
-    url: 'https://api.novaposhta.ua/v2.0/json/',
-    data: JSON.stringify({
+cityOptions.addEventListener('input', function() {
+  const city = cityOptions.value;
+  postOptions.innerHTML = '';
+
+  const API_KEY = 'd2f1d924423a9a17e82873f4e6922804';
+  const LIMIT = 555;
+  const API_URL = `https://api.novaposhta.ua/v2.0/json/`;
+
+  fetch(API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
       modelName: 'Address',
       calledMethod: 'getWarehouses',
       methodProperties: {
         CityName: city,
-        Limit: 555
+        Limit: LIMIT
       },
-      apiKey: 'd2f1d924423a9a17e82873f4e6922804'
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    xhrFields: {
-      withCredentials: false 
-    },
-    success: function(texts) {
-      let text = texts.data
-      
-      text.forEach(function(entry) {
-        dataForm.innerHTML += `<option value="${entry.Description}">${entry.Description}</option>`
-      });
-    },
-  });
-})
+      apiKey: API_KEY
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    data.data.forEach(entry => {
+      addPostOption(entry.Description);
+    });
+  })
+  .catch(error => console.error(error));
+});
+
+function addPostOption(description) {
+  const option = document.createElement('option');
+  option.value = description;
+  option.textContent = description;
+  postOptions.appendChild(option);
+}
